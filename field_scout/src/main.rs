621668e484ec83af0b3c798e47fc3911e14d41b6
lewistorrington <lewis.torrington@upcloud.com>
2024-02-13 20:13:13 +0200

    feat: full data pipeline works together

--- field_scout/src/main.rs
+++ field_scout/src/main.rs
@@ -1,24 +1,58 @@
 use std::{path::{Path, PathBuf}, time::Duration};
 
+use futures::executor;
+
+use minio::s3::args::{BucketExistsArgs, MakeBucketArgs, UploadObjectArgs};
+use minio::s3::client::Client;
+// use minio::s3::creds::StaticProvider;
+use minio::s3::http::BaseUrl;
+
 use notify::{RecursiveMode, Watcher};
 use notify_debouncer_full::new_debouncer;
 
 const IMAGE_EXTENSIONS: [&str; 4] = ["png", "jpg", "jpeg", "bmp"];
+const DEFAULT_OBJSTO_CLIENT_NAME: &str = "http://localhost:9000";
+const DEFAULT_OBJSTO_BUCKET_NAME: &str = "wru-bucket";
 
-fn main() {
-    // setup debouncer
+#[tokio::main]
+async fn main() {
+    // set up debouncer
     let (tx, rx) = std::sync::mpsc::channel();
 
     // no specific tickrate, max debounce time 1 second
     let mut debouncer = new_debouncer(Duration::from_secs(1), None, tx).unwrap();
-
     debouncer.watcher().watch(Path::new("."), RecursiveMode::Recursive).unwrap();
+
+    // set up minio conn
+    let base_url = DEFAULT_OBJSTO_CLIENT_NAME.parse::<BaseUrl>().unwrap_or_default();
+
+    // TODO: get variables from args, incl. creds
+
+    // let static_provider = StaticProvider::new(
+    //     "Q3AM3UQ867SPQQA43P2F",
+    //     "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+    //     None,
+    // );
+    let client = Client::new(
+        base_url.clone(),
+        None,
+        None,
+        None,
+    )
+    .unwrap();
+
+    let bucket_name = DEFAULT_OBJSTO_BUCKET_NAME;
 
     for result in rx {
         match result {
             Ok(events) => events.iter().for_each(|event| {
                 if event.kind.is_create() {
-                    event.paths.iter().for_each(|path| handle_created_path(path));
+                    event.paths.iter().for_each(|path| {
+                        if is_an_image(path) {
+                            // TODO: don't explode if MinIO server is down
+                            let _ = executor::block_on(upload_object(&client, bucket_name, path));
+                        }
+                    });
                 }
             }),
             Err(errors) => errors.iter().for_each(|error| println!("{error:?}")),
@@ -26,15 +60,37 @@ fn main() {
     }
 }
 
-fn handle_created_path(path_buf: &PathBuf) {
-    if is_an_image(path_buf) {
-        send_file_to_objsto(path_buf)
-    }
-}
+async fn upload_object(client: &Client, bucket_name: &str, path_buf: &PathBuf) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let path_as_str = path_buf.to_str().unwrap_or_default();
+    let file_name_in_bucket = path_as_str;
+    // Check if bucket exists or not
+    let exists = client
+        .bucket_exists(&BucketExistsArgs::new(&bucket_name).unwrap())
+        .await
+        .unwrap();
 
-fn send_file_to_objsto(path_buf: &PathBuf) {
-    println!("TODO: Sending {} to objsto", path_buf.to_str().unwrap_or_default());
-    // TODO: Send to Object Storage here!
+    // Make bucket if doesn't exist
+    if !exists {
+        client
+            .make_bucket(&MakeBucketArgs::new(&bucket_name).unwrap())
+            .await
+            .unwrap();
+    }
+
+    client
+        .upload_object(
+            &mut UploadObjectArgs::new(
+                &bucket_name,
+                file_name_in_bucket,
+                path_as_str,
+            )
+            .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    println!("{path_as_str} is successfully uploaded as object {file_name_in_bucket} to {bucket_name}.");
+    Ok(())
 }
 
 fn is_an_image(path: &PathBuf) -> bool {
--- processing/main.py
+++ processing/main.py
@@ -1,62 +1,26 @@
 #!/usr/bin/env python
 import json
-import sys
 import os
 import requests
 from io import BytesIO
 
 import pika
 import face_recognition
-from dotenv import load_dotenv
+
+import signal
 
 
-print(
-    """
-    How this program is going:
-
-    ✅\tread in from MQ
-    ✅\tdownload image from objsto based on MQ message
-    ⚙️\tWIP: extract meaning from it (object, human, face, etc. recognition)
-    ⚠️\tTODO: save results to a DB
-    ✅\trepeat
-"""
-)
-
-
-def main():
-    load_dotenv()
-
-    rmq_creds = pika.credentials.PlainCredentials(
-        os.getenv("RABBIT_MQ_USERNAME"), os.getenv("RABBIT_MQ_PASSWORD")
-    )
-
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=os.getenv("RABBIT_MQ_HOST"), credentials=rmq_creds
-        )
-    )
-    channel = connection.channel()
-
-    channel.queue_declare(queue="wru-queue", durable=True)
-
-    channel.basic_consume(
-        queue="wru-queue", on_message_callback=callback, auto_ack=True
-    )
-
-    print("👂 Waiting for messages. To exit press CTRL+C")
-    channel.start_consuming()
+RABBIT_MQ_HOST = os.getenv("RABBIT_MQ_HOST")
+RABBIT_MQ_USERNAME = os.getenv("RABBIT_MQ_USERNAME")
+RABBIT_MQ_PASSWORD = os.getenv("RABBIT_MQ_PASSWORD")
+RABBIT_MQ_QUEUE_NAME = os.getenv("RABBIT_MQ_QUEUE_NAME")
+MINIO_BASE_URL = os.getenv("MINIO_BASE_URL")
 
 
 def callback(ch, method, properties, body):
     body_dict = json.loads(body.decode("utf8"))
 
-    # Try to get the objsto base URL based on the complex JSON that MinIO provides, or default to localhost
-    objsto_base_url = (
-        body_dict.get("Records", [{}])[0]
-        .get("responseElements", {})
-        .get("x-minio-origin-endpoint", None)
-        or "http://localhost:9000"
-    )
+    objsto_base_url = MINIO_BASE_URL
 
     file_url = f"{objsto_base_url}/{body_dict['Key']}"
     print(f"New file: {file_url}")
@@ -67,12 +31,44 @@ def callback(ch, method, properties, body):
     print(f"Face locations: {face_locations}")
 
 
+def main():
+    print(
+    """
+        How this program is going:
+
+        ✅\tread in from MQ
+        ✅\tdownload image from objsto based on MQ message
+        ⚙️\tWIP: extract meaning from it (object, human, face, etc. recognition)
+        ⚠️\tTODO: save results to a DB
+        ✅\trepeat
+    """
+    )
+
+    rmq_creds = pika.credentials.PlainCredentials(
+        RABBIT_MQ_USERNAME, RABBIT_MQ_PASSWORD
+    )
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=RABBIT_MQ_HOST, credentials=rmq_creds
+        )
+    )
+
+    def handle_sigterm(*args):
+        connection.close()
+        raise KeyboardInterrupt()
+
+    signal.signal(signal.SIGTERM, handle_sigterm)
+
+    channel = connection.channel()
+    channel.queue_declare(queue=RABBIT_MQ_QUEUE_NAME, durable=True)
+    channel.basic_consume(
+        queue=RABBIT_MQ_QUEUE_NAME, on_message_callback=callback, auto_ack=True
+    )
+
+    print("👂 Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Interrupted")
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+    main()
